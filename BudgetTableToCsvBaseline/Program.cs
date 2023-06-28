55f889190b8542c1aaaa19bf5bcef94311bdac01
Ahmed El Alaoui <a.elalaoui@brink.nl>
2023-06-28 14:33:52 +0200

    Cleanup

--- BudgetTableToCsvBaseline/Program.cs
+++ BudgetTableToCsvBaseline/Program.cs
@@ -1,114 +1,53 @@
 ﻿using HtmlAgilityPack; 
 using CsvHelper; 
-using System.Globalization;
-using System.Text;
-using ConsoleTableExt;
 using CsvHelper.Configuration;
+using System.Globalization;
+using ConsoleTableExt;
 
 namespace BudgetTableToCsvBaseline 
 { 
-    public class Program 
-    { 
-        static void ParseHtml(string htmlData) {
+    public static class Program 
+    {
+        private static void ConvertBudgetTableToCsv(string htmlData, string csvPath) {
             try {
                 Console.WriteLine("### Started Parsing HTML.");
-                var coinData = new Dictionary < string,
-                    string > ();
-                HtmlDocument htmlDoc = new HtmlDocument();
+                var htmlDoc = new HtmlDocument();
                 htmlDoc.LoadHtml(htmlData);
 
-                //var theTable = htmlDoc.DocumentNode.SelectSingleNode("table");
-                //var cmcTableHeader = htmlDoc.DocumentNode.SelectNodes("//table/thead/tr/th");
-                //var cmcTableBody = theTable.SelectSingleNode("//tbody");
-                //var cmcTableRows = cmcTableBody.SelectNodes("tr");
-                // if (cmcTableRows != null) {
-                //     foreach(HtmlNode row in cmcTableRows) {
-                //         var cmcTableColumns = row.SelectNodes("td");
-                //         string name = cmcTableColumns[2].InnerText;
-                //         string price = cmcTableColumns[3].InnerText;
-                //         coinData.Add(name, price);
-                //     }
-                // }
-
                 var tableHeader = htmlDoc.DocumentNode.SelectNodes("//table/thead/tr/th");
-                List<string> tableHeaderTexts = tableHeader.Select(x => x.InnerText.Trim()).ToList();
+                var tableHeaderTexts = tableHeader.Select(x => x.InnerText.Trim()).ToList();
                 var countHeaderColumns = tableHeader.Count;
-
 
                 var tableRows = htmlDoc.DocumentNode.SelectNodes("//table/tbody/tr");
                 var countRows = tableRows.Count;
-                //var tableCellTexts = new List<string[]>(countRows) { new string[countHeaderColumns] };
                 var tableCellTexts = new List<List<object>>(countRows) { new(countHeaderColumns) };
-                //tableCellTexts = new List<string[]> { new string[10] };
-                int indexRow = 1;
+
+                var indexRow = 1;
                 foreach (var row in tableRows)
                 {
-                    int indexCell = 0;
+                    var indexCell = 0;
                     foreach (var cell in row.SelectNodes($"//table/tbody/tr[{indexRow}]/td"))
                     {
-                        //tableCellTexts[indexRow-1][indexCell] = cell.InnerText;
                         tableCellTexts[indexRow-1].Add(cell.InnerText.Trim());
                         indexCell++;
                     }
                     indexRow++;
                     tableCellTexts.Add(new List<object>());
                 }
-                // List<string> tableRowTexts = htmlDoc.DocumentNode.SelectNodes("//table/tbody/tr/td")
-                //     .Select(x => x.InnerText)
-                //     .ToList();
                 
                 ConsoleTableBuilder
                     .From(tableCellTexts)
                     .WithColumn(tableHeaderTexts)
                     .ExportAndWriteLine();
 
-                // var tableData = new List<List<object>>
-                // {
-                //     new List<object>{ "Sakura Yamamoto", "Support Engineer", "London", 46},
-                //     new List<object>{ "Serge Baldwin", "Data Coordinator", "San Francisco", 28, "rowRecords else" },
-                //     new List<object>{ "Shad Decker", "Regional Director", "Edinburgh"},
-                // };
-                //
-                // ConsoleTableBuilder
-                //     .From(tableData)
-                //     .WithColumn("Id", "First Name", "Sur Name")
-                //     .ExportAndWriteLine();
-
-                /*using var writer = new StreamWriter("C:\\Temp2\\bok.csv");
-                var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
-                {
-                    Delimiter = ";",
-                    Comment = '#',
-                    HasHeaderRecord = true,
-                    MissingFieldFound = null
-                };
+                WriteCsv(csvPath, tableCellTexts, tableHeaderTexts);
                 
-                //using var csv = new CsvWriter(writer, configuration);
-                // populating the CSV file 
-                //csv.WriteRecord(tableHeaderTexts);
-                using (var csv = new CsvWriter(writer, configuration))
-                {
-                    // csv.WriteRecord(tableHeaderTexts);
-                    // csv.NextRecord();
-
-                    foreach(var cellsFromRow in tableCellTexts) {
-                        csv.WriteRecord(cellsFromRow);
-                    }
-                    //csv.WriteRecords(tableCellTexts);
-                }
-                //csv.WriteRecords(tableCellTexts);
-                writer.Close();*/
-
-                WriteMeSomeCsv("C:\\Temp2\\bok2.csv", tableCellTexts, tableHeaderTexts);
-                
-                //WriteDataToCsv(coinData);
             } catch (Exception ex) {
-                Console.WriteLine("ParseHtml Failed: {0}", ex.Message);
+                Console.WriteLine("ConvertBudgetTableToCsv Failed: {0}", ex.Message);
             }
         }
 
-
-        public static void WriteMeSomeCsv(string file, List<List<object>> rowRecords, List<string> tableHeader)
+        private static void WriteCsv(string file, List<List<object>> rowRecords, List<string> tableHeader)
         {
             var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
@@ -138,25 +77,11 @@ namespace BudgetTableToCsvBaseline
             }
         }
         
-        /*static void WriteDataToCsv(Dictionary < string, string > cryptoCurrencyData) {
-            try {
-                var csvBuilder = new StringBuilder();
-
-                csvBuilder.AppendLine("Name,Price");
-                foreach(var item in cryptoCurrencyData) {
-                    csvBuilder.AppendLine(string.Format("{0},\"{1}\"", item.Key, item.Value));
-                }
-                File.WriteAllText("C:\\Cryptocurrency-Prices.csv", csvBuilder.ToString());
-
-                Console.WriteLine("### Completed Writing Data To CSV File.");
-            } catch (Exception ex) {
-                Console.WriteLine("WriteDataToCSV Failed: {0}", ex.Message);
-            }
-        }*/
-        
         static async Task Main(string[] args) {
-            string htmlText = await File.ReadAllTextAsync("C:\\Temp2\\aok.html");
-            ParseHtml(htmlText);
+            var htmlText = await File.ReadAllTextAsync("C:\\Temp\\budget_table.html");
+            var csvBaseline = "C:\\Temp\\budget_table.csv";
+            
+            ConvertBudgetTableToCsv(htmlText,csvBaseline);
         }
     } 
 }
